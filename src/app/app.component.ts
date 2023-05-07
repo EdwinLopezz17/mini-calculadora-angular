f4508402178eb66ce34f0adc146356a696207162
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-05-06 22:29:57 -0500

    Mini Calculadora. Comunicacion entre HTML y Servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadora';
+  title = 'Calculadora';
+  num1:number=0;
+  num2:number=0;
+  resul:number=0;
+
+  sumar():void{
+    this.resul=this.num1+this.num2;
+  }
+  restar():void{
+    this.resul=this.num1-this.num2;
+  }
+  producto():void{
+    this.resul=this.num1*this.num2;
+  }
 }
